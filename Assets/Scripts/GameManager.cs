b0621b3db2e2937a63a7acbab551f50979fb4a30
Ishaia <141527323+ishaiaa@users.noreply.github.com>
2024-02-07 20:14:03 +0100

    UI basics

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.SetResolution(1920, 1080, true);
     }
 
     // Update is called once per frame
